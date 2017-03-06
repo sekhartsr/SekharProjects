8ab7ea262a974cf7f819d390be00eddb55ef17f0
Sekhar <sekhartsr@gmail.com>
2017-03-06 22:20:02 +0800

    added files for both frontend and backend

---
+++ frontend/src/main/frontend/src/app/component/user.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [PostsService]
+})
+export class UserComponent {
+  name: string;
+  email: string;
+  address: address;
+  hobbies: string[];
+  showHobbies: boolean;
+  posts: Post[];
+
+  constructor(private postsService : PostsService) {
+    this.name = 'Sekhar';
+    this.email = 'sekhartsr@gmail.com';
+    this.address = {
+      street: 'Thuvvachilaka',
+      city: 'Vissannapeta',
+      state: 'AP'
+    }
+    this.hobbies = ['Music', 'Movies', 'Sports'];
+    this.showHobbies = false;
+    this.postsService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    })
+  }
+
+  toggleHobbies() {
+    if (this.showHobbies == true) {
+      this.showHobbies = false;
+    } else {
+      this.showHobbies = true;
+    }
+  }
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.push(hobby);
+  }
+  deleteHobby(i){
+    this.hobbies.splice(i, 1);
+  }
+}
+
+interface address {
+  street: string;
+  city: string;
+  state: string;
+
+}
+interface Post{
+  id: number;
+  title: string;
+  body: string;
+}
