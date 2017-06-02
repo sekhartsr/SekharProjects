b7de8171255be6ee665472f7002a031b18927f89
Sekhar Tunuri <sekhar.tunuri@bestinet.com.my>
2017-06-02 11:58:04 +0800

    First changes in test

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing }  from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './component/user.component';
 import { AboutComponent }  from './component/about.component';
-import { routing }  from './app.routing';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule,  routing ],
